e66bd920390b99e929ada7ba8d96b222403748b8
seathebass <sebtorres7021@gmail.com>
2020-09-17 15:53:26 -0400

    update package for the deidentify text methods using cyphr

--- R/deidentify_methods.R
+++ R/deidentify_methods.R
@@ -1,38 +1,49 @@
 #'@title methods to deidentify different forms of data
-#'
-#'
-#'
-
-
-
-
-
-
-
-# fixing up the progress bar so that it responds to the name of the column
-create_prog <- function(vect){
-  x <- length(vect)
-  progress::progress_bar$new(total = x,format = " Encrypting column :what [:bar] :percent eta: :eta")
+#'@details
+#'@param .data
+#'a data frame with columns to encrypt
+#'@param cols_to_encrypt
+#'A character vector with the names associated with the columns to encrypt
+#'@param key
+#'A cyphr key generated from the cyphr package
+#'@return
+#'A Dataframe with encrypted columns ending with the prefix 'encrypt_id'
+#'@examples
+#'\dontrun{
+#'}
+#'@export
+deidentify_text <- function(.data,cols_to_encrypt,key = NULL){
+  if(!is.character(cols_to_encrypt)){
+    stop("cols_to_encrypt must be a character vector!")
+  }
+  if(class(key) != "cyphr_key"){
+    stop("key must be a cyphr key")
+  }
+  .data[,cols_to_encrypt,drop = F] %>%
+    dplyr::mutate(dplyr::across(everything(),list("encrypt_id" = ~.apply_enc(as.character(.),key = key))))
 }
 
 
-enc_strings <- function(col_to_encrypt,name_of_col,key) {
-  pb$tick(tokens = list(what = name_of_col))
+
+.enc_strings <- function(col_to_encrypt,key) {
   # encrypts the vector depending on the key
-  enc_string <- paste0(cyphr::encrypt_string(x,key = key), collapse = "")
+  enc_string <- paste0(cyphr::encrypt_string(col_to_encrypt,key = key), collapse = "")
+  return(enc_string)
 }
 
 
 # this encrypts the unique vector of strings and passes it on.
 
-function(string_to_encrypt, key) {
-
+.apply_enc <- function(string_to_encrypt, key) {
+  unique_ent <- unique(string_to_encrypt)
   enc_string <-
-    unlist(lapply(string_to_encrypt, function(x)
-      enc_strings(x, , key)))
+    unlist(lapply(unique_ent, function(x)
+      .enc_strings(x, key)))
   # change the names based on the original encrypted string
-  names(enc_string) <- string_to_encrypt
+  names(enc_string) <- unique_ent
   # use a lookup table to speedily replace values
   # this also removes the names of the vector
-  final_vec<- unname(enc_string[data_to_encrypt])
+  final_vec <- unname(enc_string[string_to_encrypt])
+  return(final_vec)
 }
+
