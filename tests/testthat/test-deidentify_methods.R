bf2513b77d4f50c0bb74131c64d2b8f7874130ef
seabass <seabass@system76-pc.localdomain>
2020-10-20 16:19:35 -0400

    fix up tests. Everything should pass now

--- tests/testthat/test-deidentify_methods.R
+++ tests/testthat/test-deidentify_methods.R
@@ -1,10 +1,10 @@
 # check for errors
 test_that("Errors are working as expected", {
-  expect_error(deidentify_text("cat"), "key must be a cyphr key")
-  expect_error(deidentify_text("cat",cols_to_encrypt = "t"),"Key must be provided")
-  expect_error(deidentify_text("cat","1"),"key must be a cyphr key")
-  expect_error(deidentify_text(data.frame("dog"),gen_aes_key()),"cols_to_encrypt is missing")
-  expect_error(deidentify_text(data.frame(c1 = "dog"),gen_aes_key(),"c"),"You have selected")
+  expect_error(deidentify_text("cat"), regexp = "Key must be provided*")
+  expect_error(deidentify_text("cat",cols_to_encrypt = "t"),regexp = "Key must be provided*")
+  expect_error(deidentify_text("cat",key = "1"),"key must be a cyphr key*")
+  expect_error(deidentify_text(data.frame("dog"),gen_aes_key(16)),"* \"cols_to_encrypt\" is missing*")
+  expect_error(deidentify_text(data.frame(c1 = "dog"),gen_aes_key(16),"c"),"You have selected*")
 })
 
 
@@ -18,13 +18,12 @@ test_that("Check to work that things are working as expected", {
   expect_type(deidentify_text("cat", gen_aes_key()), "character")
   expect_type(
     deidentify_text(data.frame(col1 = "cat"), gen_aes_key(), cols_to_encrypt = "col1"),
-    "data.frame"
+    "list"
   )
   # ensure that keys that are encrypted with different keys are different
-  expect_equal(deidentify_text("cat", gen_aes_key(12)) != deidentify_text("cat", gen_aes_key(24)), TRUE)
+  expect_equal(deidentify_text("cat", gen_aes_key(16)) != deidentify_text("cat", gen_aes_key(24)), TRUE)
   # data.frame checks
   expect_named(deidentify_text(data.frame(col1 = "cat"), gen_aes_key(), cols_to_encrypt = "col1"))
   expect_equal(dim(deidentify_text(data.frame(col1 = "cat"), gen_aes_key(), cols_to_encrypt = "col1")), c(1, 2))
 })
-
 
